e4ef803c8d5f8d97ddc802a681184107e32f8063
Reinnn <xopeofhopeness2004@gmail.com>
2023-12-06 13:39:50 +0630

    JSON EDITION

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,1 +1,8 @@
-export const api: string = 'https://raw.githubusercontent.com/sannlynnhtun-coding/Birds/main/Birds.json';
+// export const api: string = 'https://raw.githubusercontent.com/sannlynnhtun-coding/Birds/main/Birds.json';
+export const api: string = 'https://stone-energetic-talon.glitch.me/Birds.json';
+
+// import axios from "axios";
+
+// export let api = axios.create({
+//     baseURL : 'https://raw.githubusercontent.com/sannlynnhtun-coding/Birds/main/Birds.json'
+// })
